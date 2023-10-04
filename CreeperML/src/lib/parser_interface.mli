0b3962dc833d8435aeade53af305bf815b8eb0f7
Startcev Matvey <tozarin@mail.ru>
2023-10-05 01:55:41 +0300

    adds module into parser interface
    Signed-off-by: Startcev Matvey <tozarin@mail.ru>

--- CreeperML/src/lib/parser_interface.mli
+++ CreeperML/src/lib/parser_interface.mli
@@ -1,8 +1,10 @@
-(* parse code from given string *)
-val from_string : string -> (Parser_ast.ParserAst.program, string) result
+module ParserInterface : sig
+  (* parse code from given string *)
+  val from_string : string -> (Parser_ast.ParserAst.program, string) result
 
-(* parse code from given channel *)
-val from_channel : in_channel -> (Parser_ast.ParserAst.program, string) result
+  (* parse code from given channel *)
+  val from_channel : in_channel -> (Parser_ast.ParserAst.program, string) result
 
-(* parse code from file by given filename *)
-val from_file : string -> (Parser_ast.ParserAst.program, string) result
+  (* parse code from file by given filename *)
+  val from_file : string -> (Parser_ast.ParserAst.program, string) result
+end
