4495140bb03c747c50598b56fcb4cb156094f752
Startcev Matvey <tozarin@mail.ru>
2023-10-12 03:14:27 +0300

    adds new exapmle
    Signed-off-by: Startcev Matvey <tozarin@mail.ru>

--- CreeperML/src/main.ml
+++ CreeperML/src/main.ml
@@ -2,27 +2,41 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
+let lr =
+  let open CreeperML.Type_ast.InferTypeUtils in
+  let int_const = t_ground t_int |> with_lvls 0 0 in
+  let bool_const = t_ground t_bool |> with_lvls 0 0 in
+  let arr = t_arrow int_const bool_const |> with_lvls 0 0 in
+  let lr = t_arrow int_const arr |> with_lvls 0 0 in
+  ("lor", lr)
+
+let mi =
+  let open CreeperML.Type_ast.InferTypeUtils in
+  let int_const = t_ground t_int |> with_lvls 0 0 in
+  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
+  let mi = t_arrow int_const arr |> with_lvls 0 0 in
+  ("-", mi)
+
+let ml =
+  let open CreeperML.Type_ast.InferTypeUtils in
+  let int_const = t_ground t_int |> with_lvls 0 0 in
+  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
+  let ml = t_arrow int_const arr |> with_lvls 0 0 in
+  ("*", ml)
+
 let () =
-  match CreeperML.Parser_interface.from_string {|let (a b, c) = a|} with
-  | Ok p -> CreeperML.Parser_ast.ParserAst.show_program p |> print_endline
+  match
+    CreeperML.Parser_interface.ParserInterface.from_string
+      {|let rec fac n = if lor n 0 then 1 else n * fac (n - 1)|}
+  with
+  | Ok p -> (
+      let p = CreeperML.Infer.Infer.top_infer [ lr; mi; ml ] p in
+      match p with
+      | Ok e ->
+          List.iter
+            (fun l ->
+              CreeperML.Type_ast.TypeAst.show_typ_let_binding l
+              |> Printf.printf "%s")
+            e
+      | Error err -> print_endline err)
   | Error err -> print_endline err
-
-(*
-   let colnum pos = pos.pos_cnum - pos.pos_bol - 1
-
-   let pos_string pos =
-     let l = string_of_int pos.pos_lnum and c = string_of_int (colnum pos + 1) in
-     "line " ^ l ^ ", column " ^ c
-
-   let parse' f s =
-     let lexbuf = Lexing.from_string s in
-     try f token lexbuf
-     with Error ->
-       raise (Failure ("Parse error at " ^ pos_string lexbuf.lex_curr_p))
-
-   let parse_program s = parse' parse s
-
-   let () =
-     parse_program {|let a = 1 + 2 ** 3 * 4 - 5|}
-     |> ParserAst.show_program |> print_endline
-*)
