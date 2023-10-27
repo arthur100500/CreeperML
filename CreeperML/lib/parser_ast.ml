642bbe69c31def12481e69e8cb1bebc02ae4a51d
Startcev Matvey <tozarin@mail.ru>
2023-10-28 02:41:18 +0300

    fixes infer i guess
    Signed-off-by: Startcev Matvey <tozarin@mail.ru>

--- CreeperML/lib/parser_ast.ml
+++ CreeperML/lib/parser_ast.ml
@@ -106,8 +106,7 @@ module ParserAstUtils = struct
     match lvs with
     | [] -> b
     | hd :: tl ->
-        value b |> lets |> fun lets ->
-        build_mul_e_fun start_p end_p hd tl b |> let_body start_p end_p lets
+        build_mul_e_fun start_p end_p hd tl b |> let_body start_p end_p []
 end
 
 let%expect_test _ =
