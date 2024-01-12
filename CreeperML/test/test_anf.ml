d3f7ab5c8a1ac6410fc8ace26df47d1799df0e38
Startcev Matvey <tozarin@mail.ru>
2024-01-12 09:45:57 +0300

    updates old tests
    Signed-off-by: Startcev Matvey <tozarin@mail.ru>

--- CreeperML/test/test_anf.ml
+++ CreeperML/test/test_anf.ml
@@ -1,88 +1,22 @@
 open CreeperML
 open CreeperML.Parser_interface.ParserInterface
 open Infer.Infer
-open Type_ast.InferTypeUtils
-open Type_ast.TypeAst
-open Counter
-open Pp.PrettyPrinter
 open Indexed_ast.IndexedTypeAst
 open Closure.ClosureConvert
-open String
-open Anf.AnfConvert
-module NameMap = Map.Make (String)
-
-let pi =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let unit_const = t_ground t_unit |> with_lvls 0 0 in
-  let pi = t_arrow int_const unit_const |> with_lvls 0 0 in
-  ("print_int", pi)
-
-let lr =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let bool_const = t_ground t_bool |> with_lvls 0 0 in
-  let arr = t_arrow int_const bool_const |> with_lvls 0 0 in
-  let lr = t_arrow int_const arr |> with_lvls 0 0 in
-  ("<=", lr)
-
-let mi =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let mi = t_arrow int_const arr |> with_lvls 0 0 in
-  ("-", mi)
-
-let ml =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let ml = t_arrow int_const arr |> with_lvls 0 0 in
-  ("*", ml)
-
-let pl =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let pl = t_arrow int_const arr |> with_lvls 0 0 in
-  ("+", pl)
-
-let nm =
-  let nm = NameMap.empty in
-  let nm = NameMap.add "<=" (cnt_next ()) nm in
-  let nm = NameMap.add "-" (cnt_next ()) nm in
-  let nm = NameMap.add "*" (cnt_next ()) nm in
-  let nm = NameMap.add "+" (cnt_next ()) nm in
-  let nm = NameMap.add "print_int" (cnt_next ()) nm in
-  nm
-
-let env = [ lr; mi; ml; pi; pl ]
-let typed t a : ('a, ty) typed = { value = a; typ = t }
-
-let globals =
-  [
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TBool))
-    @@ NameMap.find "<=" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "-" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "*" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "+" nm;
-    typed (TyArrow (TyGround TInt, TyGround TUnit))
-    @@ NameMap.find "print_int" nm;
-  ]
-
-(*
-  Used functions and operators:
-    1: <= (i -> i -> b)
-    2: -  (i -> i -> i)
-    3: *  (i -> i -> i)
-    4: +  (i -> i -> i)
-    5: print_int (i -> ())
-*)
+open Anf
+open Std
+open Pp.PrettyPrinter
 
 let () =
-  match from_channel stdin with
-  | Error msg -> Printf.printf "%s" msg
-  | Ok p -> (
-      match top_infer env p with
-      | Error msg -> Printf.printf "%s" msg
-      | Ok p ->
-          index_of_typed nm p |> cf_of_index globals |> anf_of_cf
-          |> print_anf_program false |> trim |> print_endline)
+  let program = from_channel stdin in
+  let ( >>= ) = Result.bind in
+  let apply_db_renaming p = Ok (index_of_typed p) in
+  let apply_closure_convert p = Ok (cf_of_index p) in
+  let apply_anf_convert p = Ok (AnfConvert.anf_of_cf p) in
+  let apply_anf_optimizations p = Ok (AnfOptimizations.optimize_moves p) in
+  let apply_infer p = top_infer Std.typeenv p in
+  program >>= apply_infer >>= apply_db_renaming >>= apply_closure_convert
+  >>= apply_anf_convert >>= apply_anf_optimizations
+  |> function
+  | Ok x -> print_anf_program false x |> print_endline
+  | Error x -> print_endline x
--- CreeperML/test/test_closures.ml
+++ CreeperML/test/test_closures.ml
@@ -1,87 +1,18 @@
 open CreeperML
 open CreeperML.Parser_interface.ParserInterface
 open Infer.Infer
-open Type_ast.InferTypeUtils
-open Type_ast.TypeAst
-open Counter
-open Pp.PrettyPrinter
 open Indexed_ast.IndexedTypeAst
 open Closure.ClosureConvert
-open String
-module NameMap = Map.Make (String)
-
-let pi =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let unit_const = t_ground t_unit |> with_lvls 0 0 in
-  let pi = t_arrow int_const unit_const |> with_lvls 0 0 in
-  ("print_int", pi)
-
-let lr =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let bool_const = t_ground t_bool |> with_lvls 0 0 in
-  let arr = t_arrow int_const bool_const |> with_lvls 0 0 in
-  let lr = t_arrow int_const arr |> with_lvls 0 0 in
-  ("<=", lr)
-
-let mi =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let mi = t_arrow int_const arr |> with_lvls 0 0 in
-  ("-", mi)
-
-let ml =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let ml = t_arrow int_const arr |> with_lvls 0 0 in
-  ("*", ml)
-
-let pl =
-  let int_const = t_ground t_int |> with_lvls 0 0 in
-  let arr = t_arrow int_const int_const |> with_lvls 0 0 in
-  let pl = t_arrow int_const arr |> with_lvls 0 0 in
-  ("+", pl)
-
-let nm =
-  let nm = NameMap.empty in
-  let nm = NameMap.add "<=" (cnt_next ()) nm in
-  let nm = NameMap.add "-" (cnt_next ()) nm in
-  let nm = NameMap.add "*" (cnt_next ()) nm in
-  let nm = NameMap.add "+" (cnt_next ()) nm in
-  let nm = NameMap.add "print_int" (cnt_next ()) nm in
-  nm
-
-let env = [ lr; mi; ml; pi; pl ]
-let typed t a : ('a, ty) typed = { value = a; typ = t }
-
-let globals =
-  [
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TBool))
-    @@ NameMap.find "<=" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "-" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "*" nm;
-    typed (TyArrow (TyArrow (TyGround TInt, TyGround TInt), TyGround TInt))
-    @@ NameMap.find "+" nm;
-    typed (TyArrow (TyGround TInt, TyGround TUnit))
-    @@ NameMap.find "print_int" nm;
-  ]
-
-(*
-  Used functions and operators:
-    1: <= (i -> i -> b)
-    2: -  (i -> i -> i)
-    3: *  (i -> i -> i)
-    4: +  (i -> i -> i)
-    5: print_int (i -> ())
-*)
+open Std
+open Pp.PrettyPrinter
 
 let () =
-  match from_channel stdin with
-  | Error msg -> Printf.printf "%s" msg
-  | Ok p -> (
-      match top_infer env p with
-      | Error msg -> Printf.printf "%s" msg
-      | Ok p ->
-          index_of_typed nm p |> cf_of_index globals |> print_cf_program false
-          |> trim |> print_endline)
+  let program = from_channel stdin in
+  let ( >>= ) = Result.bind in
+  let apply_db_renaming p = Ok (index_of_typed p) in
+  let apply_closure_convert p = Ok (cf_of_index p) in
+  let apply_infer p = top_infer Std.typeenv p in
+  program >>= apply_infer >>= apply_db_renaming >>= apply_closure_convert
+  |> function
+  | Ok x -> print_cf_program false x |> print_endline
+  | Error x -> print_endline x
